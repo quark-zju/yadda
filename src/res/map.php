97071afc3bf572ea2288837cd52fd06c168d88b6
Jun Wu <quark@fb.com>
2017-08-01 15:40:58 -0700

    app: precompile assets

--- src/res/map.php
+++ src/res/map.php
@@ -7,11 +7,8 @@
  */
 return array(
   'names' => array(
-    'css/yadda.css' => 'a046a864',
-    'js/yadda-app.min.js' => 'eb5aaf26',
-    'vendor/codemirror/codemirror.css' => 'a7742522',
-    'vendor/codemirror/codemirror.min.js' => 'f64c7b27',
-    'vendor/codemirror/mode/coffeescript/coffeescript.min.js' => '2a057844',
+    'css/yadda.css' => '9c1b1266',
+    'js/yadda-app.min.js' => 'b0d93f55',
     'vendor/coffeescript/coffeescript.js' => '8a9e999f',
     'vendor/lodash/lodash.js' => '216cb86c',
     'vendor/moment/moment.js' => '3ec21492',
@@ -19,21 +16,14 @@ return array(
     'vendor/react/react.min.js' => '58873982',
   ),
   'symbols' => array(
-    'codemirror' => 'f64c7b27',
-    'codemirror-coffeescript' => '2a057844',
-    'codemirror-css' => 'a7742522',
     'coffeescript' => '8a9e999f',
     'lodash' => '216cb86c',
     'moment' => '3ec21492',
     'react' => '58873982',
     'react-dom' => '499f306a',
-    'yadda-css' => 'a046a864',
-    'yadda-home' => 'eb5aaf26',
+    'yadda-css' => '9c1b1266',
+    'yadda-home' => 'b0d93f55',
   ),
-  'requires' => array(
-    'a046a864' => array(
-      'codemirror-css',
-    ),
-  ),
+  'requires' => array(),
   'packages' => array(),
 );
